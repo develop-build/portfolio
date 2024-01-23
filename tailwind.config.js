0f18788fa39975d6e429905f804fc74fec2248b0
develop-build <deepak7business@gmail.com>
2024-01-23 21:20:27 +0530

    setup portfolio project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: '',
+  content: ['./src/**/*.{js,jsx,ts,tsx,html,css}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
